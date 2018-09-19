a5f4480ad3436834b3dd677f15fd5467c16ad80d
Selina Piper <selina@dpiper.de>
2018-09-19 17:22:43 +0200

    Added two different joints for Hook-base and Hook in HookBaseNode and Gamescene

--- CatNap/GameScene.swift
+++ CatNap/GameScene.swift
@@ -17,6 +17,7 @@ struct PhysicsCategory {
     static let Edge: UInt32 = 0b1000    // 00 1000
     static let Label: UInt32 = 0b10000  // 01 0000
     static let Spring: UInt32 = 0b100000// 10 0000
+    static let Hook: UInt32 = 0b1000000
 }
 
 protocol EventListenerNode {
